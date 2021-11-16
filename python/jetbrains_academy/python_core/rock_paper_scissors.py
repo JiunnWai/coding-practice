e82b50c32072bf7df7eb79c90ab6b7112385aa77
Jiunn Wai Wong <31558120+JiunnWai@users.noreply.github.com>
2021-11-16 15:11:13 -0600

    Complete stage 4 of rock paper scissors project

--- python/jetbrains_academy/python_core/rock_paper_scissors.py
+++ python/jetbrains_academy/python_core/rock_paper_scissors.py
@@ -1,18 +1,47 @@
 import random
 
 
+RATINGS_FILE = "rating.txt"
 USER = "user"
 COMPUTER = "computer"
 ROCK = "rock"
 PAPER = "paper"
 SCISSORS = "scissors"
+RATING = "!rating"
 EXIT = "!exit"
+
+
+def get_username():
+    # Ask for the user's name
+    username = input("Enter your name: ")
+    print(f"Hello, {username}")
+    return username
+
+
+def get_user_score(username, ratings_file):
+    # Get the user's score from file containing previous ratings
+    user_score = 0
+
+    ratings = open(ratings_file, "r")
+    for line in ratings:
+        name, score = line.split()
+        if name == username:
+            user_score = int(score)
+            break
+    ratings.close()
+
+    return user_score
+
+
+def print_score(score):
+    # Print a message to show the user's current score
+    print(f"Your rating: {score}")
 
 
 def get_user_choice():
     # Get user input for their choice of action
     user_input = input()
-    while user_input not in [ROCK, PAPER, SCISSORS, EXIT]:
+    while user_input not in [ROCK, PAPER, SCISSORS, RATING, EXIT]:
         print("Invalid input")
         user_input = input()
     return user_input
@@ -52,24 +81,35 @@ def find_winner(user_choice, computer_choice):
     return winner
 
 
-def print_result(winner, user_choice, computer_choice):
+def calculate_score(score, winner, user_choice, computer_choice):
     # Print a message showing the result of the game
     if winner == USER:
+        score += 100
         print(f"Well done. The computer chose {computer_choice} and failed")
     elif winner == COMPUTER:
         print(f"Sorry, but the computer chose {computer_choice}")
     else:
+        score += 50
         print(f"There is a draw ({user_choice})")
+    return score
 
 
 if __name__ == '__main__':
+    username = get_username()
+    score = get_user_score(username, RATINGS_FILE)
+
     while True:
         user_choice = get_user_choice()
 
-        if user_choice != EXIT:
+        if user_choice == RATING:
+            print_score(score)
+        elif user_choice != EXIT:
             computer_choice = get_computer_choice()
             winner = find_winner(user_choice, computer_choice)
-            print_result(winner, user_choice, computer_choice)
+            score = calculate_score(score,
+                                    winner,
+                                    user_choice,
+                                    computer_choice)
         else:
             print("Bye!")
             break
